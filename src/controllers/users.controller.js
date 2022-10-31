7f4517d41bcbb873ead1af33464f5d5ec4f6e14d
LuisHM5 <a338926@uach.mx>
2022-10-30 22:44:29 -0700

    controller signupuser fetch req body, validate data from user and print errors

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,3 +1,5 @@
+import User from "../models/User.js";
+//Render
 export const renderSigninUserForm = (req, res) => {
   try {
     res.render("users/signinForm");
@@ -9,14 +11,31 @@ export const renderSignupUserForm = (req, res) => {
   } catch (error) {}
 };
 
-export const signinUser = (req, res) => {
+//Crud
+
+export const signupUser = async (req, res) => {
   try {
+    const { name, email, password, confirm_password } = req.body;
+    const errors = [];
+    if (password != confirm_password) {
+      errors.push({ text: "Passwords do not match." });
+    }
+    if (password.length < 4) {
+      errors.push({ text: "Password must be at least 4 characters." });
+    }
+    if (errors.length > 0) {
+      res.render("users/signupForm", { errors });
+    } else {
+      const passEncrypt = User.encryptPassword(password);
+      const newUser = new User({ name, email, password: passEncrypt });
+      await newUser.save();
+      res.send("signup successfully");
+    }
   } catch (error) {}
 };
 
-export const signupUser = (req, res) => {
+export const signinUser = (req, res) => {
   try {
-    res.send("signup");
   } catch (error) {}
 };
 
